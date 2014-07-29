ad83ba85700f1975db264e8423c2213ed21e3066
Himanshu Joshi <Himanshu_Joshi@external.mckinsey.com>
2014-07-29 15:29:21 +0530

    heroku specific

--- config/application.rb
+++ config/application.rb
@@ -67,5 +67,10 @@ module Bts
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku specific configuration START
+    config.assets.initialize_on_precompile = false
+    # Heroku specific configuration END
+    
   end
 end
